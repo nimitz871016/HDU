b33adab494d822b541f56b8aba202073b40ee145
nimitz_007 <nimitz_007@163.com>
2015-10-13 21:12:10 +0800

    commit

--- 1013/1013.cpp
+++ 1013/1013.cpp
@@ -2,13 +2,31 @@
 //
 
 #include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+int *split(long a) {
+	int n = 0;
+	while (a / (long)pow(10, n) != 0)
+	{
+		n++;
 
-int *split(long n) {
-
+	}
+	long sum = a;
+	int *result = (int *)malloc(sizeof(int)*n);
+	while (n != 1) {
+		result[n] = sum - sum / (long)pow(10, n - 1)*pow(10, n - 1);
+		n--;
+	}
+	return result;
 }
 int main()
 {
-
+	int a = 12345;
+	int *b = split(a);
+	for (int i = 0; i < 5; i++) {
+		printf("%d ",b[i]);
+	}
+	getchar();
     return 0;
 }
 
